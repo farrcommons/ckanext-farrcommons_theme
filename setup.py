0e875717668f21967003bf054c8a24753e9bedc0
Matthew Gamble <matthew.gamble@gmail.com>
2015-04-14 12:22:24 +0100

    Update setup.py to install data_files (public, templates)

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ import sys, os
 
 version = '0.1'
 
+def get_data_files(directory):
+    dir = os.path.dirname(__file__)
+    abs_dir = os.path.join(dir,'ckanext/farrcommons_theme/', directory)
+    paths = []
+    for root, dirs, files in os.walk(abs_dir):
+        root = os.path.relpath(root,abs_dir)
+        paths += [os.path.join(directory,root,file) for file in files]
+    return paths
+
+template_data = get_data_files('templates/')
+public_data = get_data_files('public/')
+
 setup(
     name='ckanext-farrcommons_theme',
     version=version,
@@ -18,6 +30,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.farrcommons_theme'],
     include_package_data=True,
+    package_data={'ckanext.farrcommons_theme': public_data + template_data },
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
